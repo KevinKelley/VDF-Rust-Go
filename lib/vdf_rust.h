96f851f651420eff98afe66362aea2a13118adcc
Kevin Kelley <kevin@kelleysoft.com>
2021-05-03 08:36:41 +0800

    include rust VDF directly

---
+++ lib/vdf_rust.h
@@ -0,0 +1,22 @@
+// func New(difficulty int, input [32]byte) *VDF {
+// func (vdf *VDF) Execute() {
+// func (vdf *VDF) GetOutputChannel() chan [516]byte {
+// func (vdf *VDF) Verify(proof [516]byte) bool {
+// func (vdf *VDF) IsFinished() bool {
+// func (vdf *VDF) GetOutput() [516]byte {
+
+
+void execute(
+    unsigned int difficulty,
+    char* input,  int input_size,  /*32*/
+    char* output, int output_size,  /*516...result+proof? wait wut?*/ 
+    int   sizeInBits
+);
+
+char /*bool*/ verify(
+    unsigned int  difficulty,
+    char* input,  int input_size,  /*32*/
+    char* output, int output_size, /*516?*/
+    char* proof,  int proof_size,  /*516?*/
+    int   sizeInBits
+);
